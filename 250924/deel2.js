76131977aaa3b42e385ef6766c7e340dfe25172b
MartijnvanderHoningPXL <martijn.vanderhoning@student.pxl.be>
2024-09-25 12:28:14 +0200

    sessie

---
+++ 250924/deel2.js
@@ -0,0 +1,12 @@
+let eersteZin = "Dit is de eerste zin.";
+let tweedeZin = "Dit is de tweede zin.";
+let derdeZin = "Dit is de derde zin.";
+
+console.log(eersteZin, tweedeZin, derdeZin);
+let langeZin = eersteZin + " " + tweedeZin + " " + derdeZin;
+
+alert(langeZin);
+
+let zin = "Ik wil een koekje.";
+console.log(zin.charAt(0));
+console.log(zin.charAt(3));
---
+++ 250924/oef3.js
@@ -0,0 +1,10 @@
+let getal = prompt('Vul een willekeurig getal tussen 3 en 5 in')
+console.log(getal)
+
+if (getal < 3 || getal > 5) {
+    alert("het getal ligt buiten de 3 en 5.");
+} else {
+    alert("Het getal ligt binnen de 3 en 5.")
+}
+
+
